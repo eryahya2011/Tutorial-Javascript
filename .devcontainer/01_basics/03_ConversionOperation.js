0a71fa4341ceb128ac3665eedba359cd6189c717
Sayed Yahya Jafri <er.yahya.2011@gmail.com>
2025-03-24 17:19:49 +0000

    Conversion of Datatypes

---
+++ .devcontainer/01_basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "true"
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0 
+
+let isLoggedIn = "Yahya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => truee; 0 => false
+// "" => False
+// "Yahya" => True
+// Yahya => not defined
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
