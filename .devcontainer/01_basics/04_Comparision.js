47e41b1084ad185308ad63f350622af6b8d744ee
Sayed Yahya Jafri <er.yahya.2011@gmail.com>
2025-03-25 01:10:56 +0000

    Comparision

---
+++ .devcontainer/01_basics/04_Comparision.js
@@ -0,0 +1,20 @@
+/* console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log( 2 == 1)
+console.log(2 != 1) */
+
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// Avoid such comparision; use clean codes!
+
+// Comparison and equality check are different things!
+
+// ===
+
+console.log("2" === 2)
