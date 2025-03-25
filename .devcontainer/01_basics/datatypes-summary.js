f5e8817df7018d023a8b1df2cb9edd125f6d54ce
Sayed Yahya Jafri <er.yahya.2011@gmail.com>
2025-03-25 10:59:59 +0000

    datatypes-summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//   Primitive
+
+// 7 Catagories: String, Number, Boolearn, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId)
+
+
+
+// Reference (Non primitive)
+
+// Arrays, Objects, Functions
+
+
+
+const heros = ["shaktiman, naagraj, doga"]
+let myObj = {
+    name: "Yahya",
+    age: 14,
+}
+
+const myFunction = function(){
+     console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
